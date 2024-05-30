e51c9158252eb2676b1e8a82d84c838b7b826a5e
Zaitsev Mykola <zaitsev.nikolay11@gmail.com>
2024-05-30 19:15:53 +0300

    task2

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const task1 = (arr) => {
     return arr.reverse();
 }
 
+
+const task2 = (arr1,arr2) => {
+    const combo = arr1.concat(arr2);
+    const uniqueValues = new Set(combo);
+    return Array.from(uniqueValues)
+}
+
 console.log(task1([1,2,3,4,5]));
+console.log(task2([1, 1, 2, 3], [2, 3, 4, 4]));
