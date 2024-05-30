4ebd4baabecc58b8ce234ebe4e22f1061d9612a4
Zaitsev Mykola <zaitsev.nikolay11@gmail.com>
2024-05-30 19:30:54 +0300

    task3

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ const task2 = (arr1,arr2) => {
     return Array.from(uniqueValues)
 }
 
+
+const task3 = (students) => {
+    const totalGrade = students.reduce((total, student) => total + student.grade, 0);
+    const averageGrade = totalGrade / students.length;
+    return Number(averageGrade.toFixed(2));
+};
+
+const studentsArr = [
+    { name: "Alice", age: 20, grade: 4.5 },
+    { name: "Bob", age: 21, grade: 3.9 },
+    { name: "Charlie", age: 19, grade: 4.8 },
+];
+
 console.log(task1([1,2,3,4,5]));
 console.log(task2([1, 1, 2, 3], [2, 3, 4, 4]));
+console.log(task3(studentsArr)); 
